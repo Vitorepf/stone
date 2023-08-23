e2de3ccfea15a68112bffa8cb1e18d2ffeb0f861
Vitorepf <83190724+Vitorepf@users.noreply.github.com>
2023-08-23 09:34:53 -0300

    atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ const sr = ScrollReveal({
 
 sr.reveal('.profile__border')
 sr.reveal('.profile__name', { delay: 500 })
-sr.reveal('.profile__profession', { delay: 600 })
+sr.reveal('.profile__subtitulo', { delay: 600 })
 sr.reveal('.profile__social', { delay: 700 })
 sr.reveal('.profile__info-group', { interval: 100, delay: 700 })
 sr.reveal('.profile__buttons', { delay: 800 })
